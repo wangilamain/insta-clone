096140cb88a49bb9ec667889015d734cdc998c95
wangilamain <wangilayng@gmail.com>
2020-11-23 10:28:00 -0500

    run tests

--- insta/settings.py
+++ insta/settings.py
@@ -86,8 +86,10 @@ WSGI_APPLICATION = 'insta.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'insta',
+        'USER': 'sharon',
+    'PASSWORD':'wangilamain',
     }
 }
 
@@ -116,7 +118,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
--- instagram/tests.py
+++ instagram/tests.py
@@ -1,3 +1,4 @@
 from django.test import TestCase
+from .models import Profile,Image,Comments
 
 # Create your tests here.
